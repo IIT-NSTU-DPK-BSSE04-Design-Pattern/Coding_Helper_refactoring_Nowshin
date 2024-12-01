8c92e7016d5f4e60af636ad6f28db4e847e1c013
Nowshin <ibnatnstu@gmail.com>
2024-12-01 12:06:38 +0600

    smell: magic numbers, tight Coupling refactor: extract method, encapsulated logic for Search and ProjectReader operations to reduce direct dependencies

--- metrices/MethodCount.java
+++ metrices/MethodCount.java
@@ -1,25 +1,48 @@
 package metrices;
 
 import IO.ProjectReader;
-import java.io.IOException;
 import searching.Search;
+import java.io.IOException;
 
 public class MethodCount {
 
-    public void getTotalMethods(String projectPath, String ProjectName) throws IOException {
-        try{
-        if (ProjectName.endsWith(".java")) {
-            new Search().getFile(projectPath, ProjectName);
-        } else {
-            new Search().processProject(projectPath, ProjectName);
+    public void getTotalMethods(String projectPath, String projectName) {
+        try {
+            processProjectFiles(projectPath, projectName);
+            int totalMethods = ProjectReader.count;
+            displayTotalMethods(totalMethods);
+        } catch (Exception e) {
+            handleError(e);
+        } finally {
+            cleanupResources();
         }
-        int totalMethods = ProjectReader.count;
-        System.out.println("\tTotal methods:" + totalMethods);
+    }
+
+    private void processProjectFiles(String projectPath, String projectName) throws IOException {
+        Search search = new Search();
+        if (isJavaFile(projectName)) {
+            search.getFile(projectPath, projectName);
+        } else {
+            search.processProject(projectPath, projectName);
+        }
+    }
+
+    private boolean isJavaFile(String projectName) {
+        final String JAVA_EXTENSION = ".java";
+        return projectName.endsWith(JAVA_EXTENSION);
+    }
+
+    private void displayTotalMethods(int totalMethods) {
+        System.out.println("\tTotal methods: " + totalMethods);
+    }
+
+    private void handleError(Exception e) {
+        System.err.println("An error occurred while counting methods: " + e.getMessage());
+        e.printStackTrace();
+    }
+
+    private void cleanupResources() {
         ProjectReader.count = 0;
         Search.ProjectFileName.clear();
-    }catch(Exception e){
-    
-    
-    }}
+    }
 }
-
